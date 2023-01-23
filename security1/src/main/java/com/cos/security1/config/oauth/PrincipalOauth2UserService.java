8e4d65b6af621950e9f1ba18b6a1af5957350c32
로그 <hj_kim97@naver.com>
2023-01-24 00:09:32 +0900

    [Add]페이스북 OAuth 구현
    페이스북 OAuth 기능 추가
    OAuth 제공 파라미터가 다르기 때문에,
    OAuth2UserInfo 인터페이스로 공통으로 사용
    
    refs: #1111

--- security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -9,6 +9,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.cos.security1.config.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 
@@ -46,12 +49,23 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		// userRequest 정보 -> loadUser 함수 -> OAuth 제공회사로부터 회원프로필을 받는다.
 		OAuth2User oauth2User = super.loadUser(userRequest);
 
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("페이스북 로그인 요청");
+			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		} else {
+			System.out.println("지원하지 않는 로그인 방식입니다.");
+		}
+
 		// 회원가입 진행
 		String provider = userRequest.getClientRegistration().getRegistrationId();
-		String providerId = oauth2User.getAttribute("sub");
+		String providerId = oAuth2UserInfo.getProviderId();
 		String username = provider + "_" + providerId;
 		String password = bCryptPasswordEncoder.encode("oauth");
-		String email = oauth2User.getAttribute("email");
+		String email = oAuth2UserInfo.getEmail();
 		String role = "ROLE_USER";
 
 		User userEntity = userRepository.findByUsername(username);
